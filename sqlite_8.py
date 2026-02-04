c1c62b6e12d3121e12dedbf7de64b12ada4e9157
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2026-02-04 19:24:12 +0100

    SQL rep finished

---
+++ sqlite_8.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+
+# 1. Konekcija
+# conn = sqlite3.connect('chinook.db')
+with sqlite3.connect('chinook.db') as conn:
+
+    # 2. Cursor
+    c = conn.cursor()
+
+    # 3. EXECUTE
+    #   DELETE FROM customers
+    #   WHERE customerid = 60
+    c.execute('''
+            DELETE FROM customers
+            WHERE customerid = ?
+        ''', (62,))
+
+
+    # 3.1. dohvat -> FETCH - ako je INSERT, UPDATE i DELETE onda nema fetch*
+    # data_from_db = c.fetchall()
+    # print(data_from_db)
+
+    # 3.2. promjena u bazi -> COMMIT
+    # Ako je SELECT ili dohvat, onda nema COMMIT jer ne radimo nikakve promjene
+    # conn.commit()
+
+    # 4. CLOSE - NE TREBA JER KORISTIMO WITH
+    # c.close()
+    # conn.close()
