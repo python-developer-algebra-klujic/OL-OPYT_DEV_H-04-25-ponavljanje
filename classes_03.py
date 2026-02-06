89563177966d2d66e2361ae2b3628d0fd11af718
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2026-02-06 20:39:51 +0100

    Create classes_03.py

---
+++ classes_03.py
@@ -0,0 +1,77 @@
+
+
+class Product:
+    """
+    Proizvod s cijenom.
+
+    Polja:
+    - name: string
+    - price: int (>=0)
+    """
+
+    def __init__(self, name: str, price: int):
+        # STUDENT CODE START
+        self.name = name
+        self.price = max(0, price)
+        # self.price = price if price >= 0 else 0
+        # if price >= 0:
+        #     self.price = price
+        # else:
+        #     self.price = 0
+        # STUDENT CODE END
+
+    def apply_discount(self, percent: int) -> None:
+        """
+        Smanji cijenu za percent (%).
+        Cijena ostaje int;
+        Ako je percent <= 0, ne mijenjaj cijenu.
+        """
+        # STUDENT CODE START
+        if percent > 0:
+            # self.price -= int(self.price * (percent / 100))
+            self.price = self.price - int(self.price * (percent / 100))
+        # STUDENT CODE END
+
+
+class Cart:
+    """
+    Košarica proizvoda.
+
+    Polje:
+    - items: lista Product objekata
+    """
+
+    def __init__(self):
+        # STUDENT CODE START
+        pass
+        # STUDENT CODE END
+
+    def add(self, product: Product) -> None:
+        """Dodaj product u items."""
+        # STUDENT CODE START
+        pass
+        # STUDENT CODE END
+
+    def total(self) -> int:
+        """Vrati zbroj cijena svih proizvoda u items."""
+        # STUDENT CODE START
+        pass
+        # STUDENT CODE END
+
+
+
+# Product
+p1 = Product("Kruh", 2)
+p2 = Product("Sir", 10)
+assert p1.name == "Kruh" and p1.price == 2
+p2.apply_discount(10)
+assert p2.price == 9
+p2.apply_discount(0)
+assert p2.price == 9
+
+
+# Cart
+c = Cart()
+c.add(p1)
+c.add(p2)
+assert c.total() == 11
